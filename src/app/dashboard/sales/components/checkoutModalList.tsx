4256452e7935e8908087474159eba6b6fc6e3df7
Caio Freire <caiohenriquefreire@icloud.com>
2023-08-04 11:52:59 -0300

    verification submit form

--- src/app/dashboard/sales/components/checkoutModalList.tsx
+++ src/app/dashboard/sales/components/checkoutModalList.tsx
@@ -18,9 +18,13 @@ export const CheckoutModalList: React.FC<CheckoutModalProps> = ({ isOpen, onClos
   const [discountPercent, setDiscountPercent] = useState(0);
   const [discountAmount, setDiscountAmount] = useState(0);
   const [troco, setTroco] = useState(0);
+  const [totalValue, setTotalValue] = useState<number>(calculateTotal() - discountAmount);
+
+  const [errorPaymentMethod, setErrorPaymentMethod] = useState(false);
+  const [errorTotal, setErrorTotal] = useState(false);
 
   const total = calculateTotal() - discountAmount;
-
+  const totalGross = formatCurrency(calculateTotal());
 
   const handlePaymentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const paymentValue = parseFloat(event.target.value);
@@ -41,11 +45,11 @@ export const CheckoutModalList: React.FC<CheckoutModalProps> = ({ isOpen, onClos
     const paymentMethod = event.target.value;
     setSelectedPaymentMethod(paymentMethod);
 
-    if (paymentMethod !== "money") {
+    if (paymentMethod === "money") {
+      setChange(calculateChange());
+    } else {
       setChange(0);
       setTroco(0);
-    } else {
-      setTroco(calculateChange());
     }
   };
 
@@ -63,6 +67,7 @@ export const CheckoutModalList: React.FC<CheckoutModalProps> = ({ isOpen, onClos
     const discountAmountValue = (calculateTotal() * discountPercentValue) / 100;
     setDiscountAmount(discountAmountValue);
     setChange(calculateTotal() - discountAmountValue);
+    setTotalValue(calculateTotal() - discountAmountValue);
   }
 
   const formatValueToTwoDecimals = (value: number) => {
@@ -70,15 +75,26 @@ export const CheckoutModalList: React.FC<CheckoutModalProps> = ({ isOpen, onClos
   }
 
   const calculateChange = () => {
-    const changeValue = calculateTotal() - discountAmount;
-    const change = payment - changeValue;
-    return selectedPaymentMethod === 'money' ? (change > 0 ? formatValueToTwoDecimals(change) : formatValueToTwoDecimals(0)) : formatValueToTwoDecimals(0);
+    const changeValue = payment - total;
+    return selectedPaymentMethod === 'money' ? (changeValue > 0 ? formatValueToTwoDecimals(changeValue) : formatValueToTwoDecimals(0)) : formatValueToTwoDecimals(0);
   };
 
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+
+    const isTotalInvalid = calculateTotal() <= 0;
+    const isPaymentMethodInvalid = selectedPaymentMethod === '';
+    const isPaymentInvalid = selectedPaymentMethod === 'money' && (isNaN(payment) || payment <= 0);
+
+    setErrorTotal(isTotalInvalid);
+    setErrorPaymentMethod(isPaymentMethodInvalid || isPaymentInvalid);
+
+    if (isTotalInvalid || isPaymentMethodInvalid || isPaymentInvalid) {
+      return;
+    }
+
     onFormSubmit?.();
-    onClose?.()
+    onClose?.();
   }
 
   return (
@@ -91,14 +107,17 @@ export const CheckoutModalList: React.FC<CheckoutModalProps> = ({ isOpen, onClos
         <li
           className='flex justify-between items-center border-b'>
           Total Bruto
-          <span className='bg-backgroundFields px-9 py-2 mb-4 rounded-lg'>{formatCurrency(calculateTotal())}</span>
+          <span
+            className={`bg-backgroundFields px-9 py-2 mb-4 rounded-lg border ${errorTotal ? "border-red-800" : "border-transparent"}`}>
+            {formatCurrency(totalGross)}
+          </span>
         </li>
 
         {/* Desconto Percentual */}
         <li
           className='flex justify-between items-center border-b'>
           Desconto %
-          <span className='bg-backgroundFields w- px-8 py-2 mb-4 rounded-lg'>
+          <span className='bg-backgroundFields w- px-8 py-2 mb-4 border border-transparent rounded-lg'>
             <input
               type="number"
               id="descount"
@@ -113,7 +132,7 @@ export const CheckoutModalList: React.FC<CheckoutModalProps> = ({ isOpen, onClos
         <li
           className='flex justify-between items-center border-b'>
           Desconto R$
-          <span className='bg-backgroundFields px-6 py-2 mb-4 rounded-lg'>
+          <span className='bg-backgroundFields px-6 py-2 mb-4 border border-transparent rounded-lg'>
             R$ <input
               type="number"
               id="discount"
@@ -127,21 +146,22 @@ export const CheckoutModalList: React.FC<CheckoutModalProps> = ({ isOpen, onClos
         <li
           className='flex justify-between items-center border-b'>
           Acréscimo
-          <span className='bg-backgroundFields px-8 py-2 mb-4 rounded-lg'>{formatCurrency(0)}</span>
+          <span className='bg-backgroundFields px-8 py-2 mb-4 border border-transparent rounded-lg'>{formatCurrency(0)}</span>
         </li>
 
         {/* Total */}
         <li
           className='flex justify-between items-center border-b'>
           Total
-          <span className='bg-backgroundFields px-8 py-2 mb-4 rounded-lg'>{formatCurrency(calculateTotal() - discountAmount)}</span>
+          <span className='bg-backgroundFields px-8 py-2 mb-4 border border-transparent rounded-lg'>{isNaN(totalValue) ? formatCurrency(calculateTotal()) : formatCurrency(totalValue)}</span>
         </li>
 
         {/* Forma de Pagamento */}
         <li
           className='flex justify-between items-center border-b'>
           Forma de Pagamento
-          <span className='bg-backgroundFields px-6 py-2 mb-4 rounded-lg'>
+          <span
+            className={`bg-backgroundFields px-6 py-2 mb-4 rounded-lg border ${errorPaymentMethod ? "border-red-800" : "border-transparent"}`}>
             <select
               className="bg-transparent appearance-none border-none"
               onChange={handlePaymentMethodChange}
@@ -172,7 +192,7 @@ export const CheckoutModalList: React.FC<CheckoutModalProps> = ({ isOpen, onClos
         {selectedPaymentMethod === "money" ? (
           <li className='flex justify-between items-center border-b'>
             Pagamento
-            <span className='bg-backgroundFields px-6 py-2 mb-4 rounded-lg'>
+            <span className='bg-backgroundFields px-6 py-2 mb-4 border border-transparent rounded-lg'>
               R$ <input
                 type="number"
                 id="descount"
@@ -184,7 +204,7 @@ export const CheckoutModalList: React.FC<CheckoutModalProps> = ({ isOpen, onClos
         ) : (
           <li className='flex justify-between items-center border-b'>
             Pagamento
-            <span className='bg-backgroundFields px-6 py-2 mb-4 rounded-lg'>
+            <span className='bg-backgroundFields px-6 py-2 mb-4 border border-transparent rounded-lg'>
               R$ <input
                 type="number"
                 id="descount"
@@ -202,7 +222,7 @@ export const CheckoutModalList: React.FC<CheckoutModalProps> = ({ isOpen, onClos
             className='flex justify-between items-center border-b'>
             Troco
             <span
-              className='bg-backgroundFields px-8 py-2 mb-4 rounded-lg'>
+              className='bg-backgroundFields px-8 py-2 mb-4 border border-transparent rounded-lg'>
               {formatCurrency(calculateChange())}
             </span>
           </li>
@@ -211,19 +231,18 @@ export const CheckoutModalList: React.FC<CheckoutModalProps> = ({ isOpen, onClos
             className='flex justify-between items-center border-b'>
             Troco
             <span
-              className='bg-backgroundFields px-8 py-2 mb-4 rounded-lg'>
+              className='bg-backgroundFields px-8 py-2 mb-4 border border-transparent rounded-lg'>
               {formatCurrency(0)}
             </span>
           </li>
         )}
 
-
       </ul>
 
       <button
         type="submit"
-        className='fixed bottom-0 flex w-full justify-center items-center rounded-b-2xl py-4 bg-emerald-900'
-        >Finalizar Venda
+        className='fixed bottom-0 flex w-full justify-center items-center rounded-b-2xl py-4 transition-all bg-emerald-900 hover:bg-emerald-700'
+      >Finalizar Venda
       </button>
     </form>
   )
