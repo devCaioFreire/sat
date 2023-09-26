5f382ff9c7e241a1a47ef2e91caed3cec8d67e23
Caio Freire <caiohenriquefreire@icloud.com>
2023-09-26 09:31:05 -0300

    only black (calendar)

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,11 +1,11 @@
 "use client"
 
-import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
+import * as React from "react"
 import { DayPicker } from "react-day-picker"
 
-import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
@@ -34,20 +34,20 @@ function Calendar({
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
         head_cell:
-          "text-stone-500 rounded-md w-9 font-normal text-[0.8rem] dark:text-stone-400",
+          "rounded-md w-9 font-normal text-[0.8rem] text-stone-400",
         row: "flex w-full mt-2",
-        cell: "text-center text-sm p-0 relative [&:has([aria-selected])]:bg-stone-100 first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20 dark:[&:has([aria-selected])]:bg-stone-800",
+        cell: "text-center text-sm p-0 relative first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-stone-800",
         day: cn(
           buttonVariants({ variant: "ghost" }),
           "h-9 w-9 p-0 font-normal aria-selected:opacity-100"
         ),
         day_selected:
-          "bg-stone-900 text-stone-50 hover:bg-stone-900 hover:text-stone-50 focus:bg-stone-900 focus:text-stone-50 dark:bg-stone-50 dark:text-stone-900 dark:hover:bg-stone-50 dark:hover:text-stone-900 dark:focus:bg-stone-50 dark:focus:text-stone-900",
-        day_today: "bg-stone-100 text-stone-900 dark:bg-stone-800 dark:text-stone-50",
-        day_outside: "text-stone-500 opacity-50 dark:text-stone-400",
-        day_disabled: "text-stone-500 opacity-50 dark:text-stone-400",
+          "focus:text-stone-50 bg-stone-50 text-stone-900 hover:bg-stone-50 hover:text-stone-900 focus:bg-stone-50 focus:text-stone-900",
+        day_today: "bg-stone-800 text-stone-50",
+        day_outside: "opacity-50 text-stone-400",
+        day_disabled: "opacity-50 text-stone-400",
         day_range_middle:
-          "aria-selected:bg-stone-100 aria-selected:text-stone-900 dark:aria-selected:bg-stone-800 dark:aria-selected:text-stone-50",
+          "aria-selected:text-stone-900 aria-selected:text-stone-50",
         day_hidden: "invisible",
         ...classNames,
       }}
@@ -62,3 +62,4 @@ function Calendar({
 Calendar.displayName = "Calendar"
 
 export { Calendar }
+
