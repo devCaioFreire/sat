7c2457e46ea709ac45bfee62aa5aa4224cb02395
Caio Freire <caiohenriquefreire@icloud.com>
2023-10-26 11:23:45 -0300

    Create toastify.tsx

---
+++ src/components/toastify.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+// minified version is also included
+// import 'react-toastify/dist/ReactToastify.min.css';
+
+export function Toastify() {
+
+  return (
+    <ToastContainer
+      position="bottom-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+    />
+  );
+}
